722dacc73e7c908591d0114fd995b3ed4098bf6f
Dominik Kasprzak <dominik.kasprzak@microsoft.wsei.edu.pl>
2022-10-15 12:44:01 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
-function App() {
+import { Home } from './Components/Home/home';
+import { Posts } from './Components/Posts/posts';
+
+export const App : FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <img src= {'${process.env.PUBLIC_URL}/logo192.png'} alt=""/> 
+        <div>
+          <>{console.log('test')}</>;
+          <Link to="Posts">Posts</Link>
+          <Link to="/">Home</Link>
+        </div>
+        <Routes>
+          <Route path='Posts' element={<Posts/>}/>
+          <Route index element = {<Home/>}/>
+        </Routes>
+    </BrowserRouter>
   );
-}
-
-export default App;
+};
